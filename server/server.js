dece69fd9ca6f2ad9585dfebd568457bfa32fffd
ramcharan2410 <ramcharanvakkalanka@gmail.com>
2024-01-21 22:12:54 +0530

    Cors fix try for Vercel deployment 2

--- server/server.js
+++ server/server.js
@@ -8,7 +8,15 @@ const app = express()
 app.use(morgan('tiny')) // console.log(details of the HTTP request)
 app.use(express.json()) // for parsing JSON requests
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
   next()
 })
 // const PORT = 3001
