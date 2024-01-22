e06ab6f0193b300085c95ee3d625aa9dbf6c47a4
ramcharan2410 <ramcharanvakkalanka@gmail.com>
2024-01-22 13:41:00 +0530

    Postgres connection successful

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 const morgan = require('morgan')
-const { Pool } = require('pg')
+const { Client } = require('pg')
+
 require('dotenv').config()
+
 const app = express()
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms')) // console.log(details of the HTTP request)
-app.use(express.json()) // for parsing JSON requests
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(express.json())
+
 const postgres_sql_user = process.env.POSTGRES_SQL_USER
 const postgres_sql_host = process.env.POSTGRES_SQL_HOST
 const postgres_sql_password = process.env.POSTGRES_SQL_PASSWORD
@@ -25,7 +29,8 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = 3001
-const pool = new Pool({
+
+const client = new Client({
   user: postgres_sql_user,
   host: postgres_sql_host,
   database: 'book-tracker',
@@ -33,40 +38,52 @@ const pool = new Pool({
   port: 5432,
 })
 
-const saltRounds = 10
 app.get('/', (req, res) => {
   res.json('Hello')
 })
+// Connect to the database when the server starts
+client.connect().then(() => {
+  console.log('Connected to the database')
+})
+
+// Middleware to make the client instance available to the request handlers
+app.use((req, res, next) => {
+  req.dbClient = client
+  next()
+})
+
+const saltRounds = 10
+
 app.post('/signup', async (req, res) => {
-  // console.log(req.body)
   const { userName, email, password } = req.body
+
   try {
-    const existingUserName = await pool.query(
+    const existingUserName = await client.query(
       'SELECT * FROM users WHERE userName = $1',
       [userName]
     )
-    const existingEmail = await pool.query(
+    const existingEmail = await client.query(
       'SELECT * FROM users WHERE email = $1',
       [email]
     )
-    if (existingUserName) {
-      console.log(existingUserName)
+
+    if (existingUserName.rows.length > 0) {
       return res.status(409).json({ message: 'UserName already exists' })
-      // 409 => Resource already exists
     }
-    if (existingEmail) {
+
+    if (existingEmail.rows.length > 0) {
       return res.status(409).json({ message: 'Email already exists' })
-      // 409 => Resource already exists
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds)
-    await pool.query(
+    await client.query(
       'INSERT INTO users (userName, email, password) VALUES ($1, $2, $3)',
       [userName, email, hashedPassword]
     )
+
     return res
       .status(200)
-      .json({ message: 'Signup successful', userName: userName, email: email })
+      .json({ message: 'Signup successful', userName, email })
   } catch (err) {
     console.error('Signup error:', err.message)
     return res
@@ -74,32 +91,32 @@ app.post('/signup', async (req, res) => {
       .json({ message: 'Signup failed: Internal Server Error' })
   }
 })
+
 app.post('/login', async (req, res) => {
-  // console.log(req.body)
   const { userName, password } = req.body
+
   try {
-    const row = await pool.query('SELECT * FROM users WHERE userName = $1', [
-      userName,
-    ])
-    console.log(row)
-    if (row.length > 0) {
-      const match = await bcrypt.compare(password, row.password)
+    const result = await client.query(
+      'SELECT * FROM users WHERE userName = $1',
+      [userName]
+    )
+
+    if (result.rows.length > 0) {
+      const match = await bcrypt.compare(password, result.rows[0].password)
 
       if (match) {
         return res.status(200).json({
           message: 'Login successful',
-          userName: row.userName,
-          email: row.email,
+          userName: result.rows[0].userName,
+          email: result.rows[0].email,
         })
       } else {
         return res
           .status(401)
           .json({ message: 'Login failed: Invalid credentials' })
-        // 401 => Unauthorized
       }
     } else {
       return res.status(404).json({ message: 'Login failed: User not found' })
-      // 404 => Not Found
     }
   } catch (err) {
     console.error('Login error:', err)
@@ -110,18 +127,20 @@ app.post('/login', async (req, res) => {
 app.post('/users/:userName/addBook', async (req, res) => {
   const { userName } = req.params
   const { bookItem } = req.body
-  // console.log(userName, bookItem)
-  const existingBook = await pool.query(
+
+  const existingBook = await client.query(
     'SELECT * FROM userBooks WHERE userName = $1 AND name = $2',
     [userName, bookItem.name]
   )
-  if (existingBook) {
+
+  if (existingBook.rows.length > 0) {
     return res.status(400).json({
       message: 'Book already exists',
     })
   }
+
   try {
-    await pool.query(
+    await client.query(
       `
       INSERT INTO userBooks (id, userName, name, author, year, pages, genre, status, pagesRead)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
@@ -138,6 +157,7 @@ app.post('/users/:userName/addBook', async (req, res) => {
         bookItem.pagesRead,
       ]
     )
+
     return res.status(200).json({ message: 'Book added successfully' })
   } catch (error) {
     console.error('Error adding book:', error)
@@ -151,26 +171,23 @@ app.get('/user/:userName/books', async (req, res) => {
   const { userName } = req.params
 
   try {
-    const books = await pool.query(
-      `
-      SELECT * FROM userBooks
-      WHERE userName = $1
-    `,
+    const books = await client.query(
+      'SELECT * FROM userBooks WHERE userName = $1',
       [userName]
     )
-
-    return res.status(200).json(books)
+    return res.status(200).json(books.rows)
   } catch (error) {
     console.error('Error fetching books:', error)
     return res.status(500).json({ error: 'Internal Server Error' })
   }
 })
+
 app.put('/users/:userName/updateBook/:id', async (req, res) => {
   const { userName, id } = req.params
   const { pagesRead, status } = req.body
 
   try {
-    await pool.query(
+    await client.query(
       `
       UPDATE userBooks
       SET pagesRead = $1,
@@ -179,17 +196,19 @@ app.put('/users/:userName/updateBook/:id', async (req, res) => {
     `,
       [pagesRead, status, userName, id]
     )
+
     return res.status(200).json({ message: 'Book updated successfully' })
   } catch (error) {
     console.error('Error updating book:', error)
     return res.status(500).json({ message: 'Internal Server Error' })
   }
 })
+
 app.delete('/users/:userName/deleteBook/:id', async (req, res) => {
   const { userName, id } = req.params
 
   try {
-    await pool.query(
+    await client.query(
       `
       DELETE FROM userBooks
       WHERE userName = $1 AND id = $2
@@ -203,6 +222,17 @@ app.delete('/users/:userName/deleteBook/:id', async (req, res) => {
     return res.status(500).json({ error: 'Internal Server Error' })
   }
 })
+// Handle cleanup during server shutdown
+app.on('close', () => {
+  client.end().then(() => console.log('Database connection closed'))
+})
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`)
+})
+// Handle server shutdown
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server is shutting down')
+    process.exit(0)
+  })
 })
